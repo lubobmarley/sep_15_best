7e7c8f5f5c9e3cafe59bb94efe2bd9f605cef406
lubopc <lubopc@lubo>
2015-04-14 16:48:07 +0200

    test

---
+++ trax_basic/main.cpp
@@ -0,0 +1,31 @@
+//------------------------------------------------------------------------------
+// main.cpp
+//
+// Group: Group 43, study assistant: Max Mustertutor
+//
+// Authors: Super Student 1 (0800001)
+// Super Student 2 (0800002)
+// Super Student 3 (0800003)
+// Super Student 4 (0800004)
+//------------------------------------------------------------------------------
+//
+
+#include <iostream>
+#include "Game.h"
+#include "Position.h"
+#include "Color.h"
+#include "Tile.h"
+ 
+//------------------------------------------------------------------------------
+// main function
+// entry point for execution
+// @param argc argument count
+// @param **argv argument values
+// @return return value for the OS
+//
+int main(int argc, char **argv)
+{
+
+  
+  return 0;
+}
